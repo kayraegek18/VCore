a62258966af734ab61805b7656bdf4fdf7393332
kayraegek18 <72798528+kayraegek18@users.noreply.github.com>
2023-08-27 16:02:20 +0300

    Delete src/main/java/net/kayega/core/config directory

--- src/main/java/net/kayega/core/config/VConfigArguments.java
+++
@@ -1,9 +0,0 @@
-package net.kayega.core.config;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface VConfigArguments {
-    String configFileName();
-}
